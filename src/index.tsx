8f78bda013b83c1a725e925263d19e43aa765b99
Ryanne Graff <ryantgraff@resistdesign.com>
2023-05-22 23:07:23 -0400

    chore: (repo) Automatic commit.

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,32 @@
-import React, {FC} from 'react';
+import React, {FC, useCallback, useState} from 'react';
 import ReactDOM from 'react-dom';
+import {createGlobalStyle} from "styled-components";
 
 const root = document.getElementById('react-root');
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+  }
+`;
+
 const App: FC = () => {
+    const [message, setMessage] = useState('Waiting...');
+    const onClick = useCallback(() => {
+        setMessage('Things are happening!!!');
+    }, setMessage);
+
     return (
-        <div>
-            App
-        </div>
+        <>
+            <GlobalStyle/>
+            <br/>
+            <div>
+                {message}
+                <button onClick={onClick}>YEY!</button>
+            </div>
+        </>
     );
 };
 
